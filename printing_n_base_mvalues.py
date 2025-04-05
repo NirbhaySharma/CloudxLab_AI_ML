a20f7745da5be832f7a4f32c9d6f628390be8f11
Nirbhay_Sharma <nirbhaysharma10@gmail.com>
2025-04-05 21:23:20 +0530

    Create printing_n_base_mvalues.py

---
+++ printing_n_base_mvalues.py
@@ -0,0 +1,27 @@
+import math
+
+n = 2
+m = 2
+
+lis = []
+for i in range(0 , n):
+    lis.append(0)
+
+print(lis)
+print_iterations = math.pow(m,n)
+
+def increment_val(lis, power):
+    carry = 1
+    for i in range( len(lis)-1,-1,-1):
+        if(lis[i] + carry >=power):
+            lis[i] = 0
+            carry = 1
+            if i == 0:
+                lis = [1] + lis
+        else:
+            lis[i] = lis[i] + 1
+            return lis
+    return lis
+
+for i in range(0,int(print_iterations-1)):
+    print(increment_val(lis,m))
