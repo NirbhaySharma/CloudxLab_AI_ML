30a3ee8996543e6f649a6b28c77b4f90b43fa1ad
Nirbhay_Sharma <nirbhaysharma10@gmail.com>
2025-04-05 20:37:32 +0530

    Create customPowFunct.py

---
+++ customPowFunct.py
@@ -0,0 +1,26 @@
+num = -2
+power = 10
+
+def multiply(x,y):
+    sum = 0
+    val = '+'
+    if (x <0 and y >0) or (x >0 and y < 0) :
+        val = '-'
+
+    for i in range(0,abs(y)):
+        sum += abs(x)
+    if val == "+":
+        return sum
+    else:
+        return -sum
+
+def pow_cust(num, power):
+    if power % 1 != 0:
+        return -1
+    numb = num
+    for i in range(0 , power-1):
+        numb = multiply(numb,num)
+    return numb
+
+
+print(pow_cust(num,power))
